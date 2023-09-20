7d4cd810e09e089a5365ebcca1806bd19ca8494f
Ts1po <115225791+Ts1po@users.noreply.github.com>
2023-09-20 22:22:40 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+lower = "qwertyuiopasdfghjklzxcvbnm"
+upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
+numbers = "1234567890"
+symbols = "[]{}()!@#$%^&*+;"
+
+all = lower + upper + numbers + symbols
+length = 14
+password = "".join(random.sample(all, length))
+print(password)
